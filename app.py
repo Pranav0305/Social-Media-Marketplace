21b30adbead6532035938fb9dba3514dbb6240fe
Annika Sinha <131460984+annikaasinha@users.noreply.github.com>
2025-02-16 00:18:43 +0530

    Update app.py
    Added functionality for viewing list of registered users

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 from pymongo import MongoClient
-from backend import models
 
 app = Flask(__name__)
 mongo = MongoClient("mongodb+srv://pranav22363:m6UrGZqKnMlcpARb@cluster0.5uvzn.mongodb.net/")
-db = mongo["App"]  
+db = mongo["App"]
+userAccounts = db["UserAccounts"]  
+userAccounts.create_index("username", unique=True)
 
 @app.route('/')
 def homePage():
@@ -12,15 +13,22 @@ def homePage():
 
 @app.route('/addUser')
 def addUser():
-    userAccounts = db["UserAccounts"] 
-    userAccounts.create_index("username", unique=True)
+    newUser = {"username": "Pranav", "password": "1234"}
     
-    newUser = models.User(username = "Pranav", password = "1234")
     try:
-        userAccounts.insert_one({"username" : newUser.username, "password" : newUser.password})
+        userAccounts.insert_one(newUser)
         return "User Added"
     except:
         return "User already exists"
+
+@app.route('/users', methods=['GET'])
+def get_registered_users():
+    try:
+        users = userAccounts.find({}, {"_id": 0, "username": 1})  
+        return jsonify(list(users))
     
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
