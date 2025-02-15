3c12c6eb1dc1b8355992e96f05d9fb0ff2097362
Pranav0305 <pranav22363@iiitd.ac.in>
2025-02-16 03:49:15 +0530

    Refactored labels, implemented base UI, admin and login functionalies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template, jsonify
 from pymongo import MongoClient
 from backend import models
 
@@ -6,21 +6,51 @@ app = Flask(__name__)
 mongo = MongoClient("mongodb+srv://pranav22363:m6UrGZqKnMlcpARb@cluster0.5uvzn.mongodb.net/")
 db = mongo["App"]  
 
-@app.route('/')
-def homePage():
-    return "Home Page"
+@app.route('/', methods = ['GET', 'POST'])
+def loginPage():
+    userAccounts = db['UserAccounts']
+    
+    if request.method == 'POST':
+        uName = request.form.get("username")
+        pwd = request.form.get("pwd")
+        if uName and pwd:
+            currUser = userAccounts.find({"username" : uName, "password" : pwd})
+            try:
+                currUserEntry = next(currUser, None)
+                print(currUserEntry)
+            except:
+                print(currUser)
+    return render_template("loginPage.html")
 
-@app.route('/addUser')
-def addUser():
-    userAccounts = db["UserAccounts"] 
-    userAccounts.create_index("username", unique=True)
+@app.route('/registerUser', methods = ['GET', 'POST'])
+def registerUser():
+    userAccounts = db["UserAccounts"]
     
-    newUser = models.User(username = "Pranav", password = "1234")
+    if request.method == 'POST':
+        uName = request.form.get("username")
+        pwd = request.form.get("pwd")
+        
+        if uName and pwd:
+            newUser = models.User(username = uName, password = pwd)
+            try:
+                userAccounts.insert_one({"username" : newUser.username, "password" : newUser.password})
+                return "User Added"
+            except:
+                return "User already exists"
+            
+    return render_template("registerUserPage.html")
+
+
+@app.route('/users', methods=['GET'])
+def get_registered_users():
+    userAccounts = db["UserAccounts"]
+    
     try:
-        userAccounts.insert_one({"username" : newUser.username, "password" : newUser.password})
-        return "User Added"
-    except:
-        return "User already exists"
-    
+        users = userAccounts.find({}, {"_id": 0, "username": 1})  
+        return jsonify(list(users))
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+  
 if __name__ == "__main__":
     app.run(host = '0.0.0.0')
--- backend/models.py
+++ backend/models.py
@@ -4,3 +4,10 @@ class User():
         self.password = password
     def __str__(self):
         return str(self.username)
+    
+class Admin():
+    def __init__(self, admin_name, admin_password):
+        self.admin_name = admin_name
+        self.admin_password = admin_password
+    def __str__(self):
+        return str(self.admin_name)
