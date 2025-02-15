0b063d4968fa78b4b54d59832aaab947ad3b8a98
Pranav0305 <pranav22363@iiitd.ac.in>
2025-02-15 16:47:12 +0000

    Created user model and setup MongoDB

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from pymongo import MongoClient
+from backend import models
+
+app = Flask(__name__)
+mongo = MongoClient("mongodb+srv://pranav22363:m6UrGZqKnMlcpARb@cluster0.5uvzn.mongodb.net/")
+
+@app.route('/')
+def homePage():
+    return "Home Page"
+
+@app.route('/addUser')
+def addUser():
+    db = mongo["App"]  
+    userAccounts = db["UserAccounts"] 
+    userAccounts.create_index("username", unique=True)
+    
+    newUser = models.User(username = "Pranav", password = "1234")
+    try:
+        userAccounts.insert_one({"username" : newUser.username, "password" : newUser.password})
+        return "User Added"
+    except:
+        return "User already exists"
+    
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0')
---
+++ backend/models.py
@@ -0,0 +1,6 @@
+class User():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    def __str__(self):
+        return str(self.username)
