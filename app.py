47145862fc119e7fccffd77c0453bfc57147c8d4
Annika Sinha <131460984+annikaasinha@users.noreply.github.com>
2025-02-27 16:26:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,42 @@
-from flask import Flask
+from flask import Flask,  send_from_directory
 from config import Config
-from extensions import mongo  
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-
-mongo.init_app(app)
-
-
+from extensions import mongo, login_manager  
 from routes.auth import auth_bp
 from routes.admin import admin_bp
 from routes.profile import profile_bp
 from routes.messaging import messaging_bp
 from routes.home import home_bp
+from flask_login import UserMixin
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.secret_key = "your_secret_key" 
+
+mongo.init_app(app)
+UPLOAD_FOLDER = 'static/uploads/'
+
+@app.route('/uploads/<filename>') 
+def uploaded_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+login_manager.init_app(app)  
+login_manager.login_view = "auth.login"  
+
+class User(UserMixin):
+    def __init__(self, user_data):
+        self.id = str(user_data["_id"])
+        self.username = user_data["username"]
+        self.email = user_data["email"]
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_data = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+    return User(user_data) if user_data else None
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(admin_bp, url_prefix='/admin')
-app.register_blueprint(profile_bp)
+app.register_blueprint(profile_bp, url_prefix='/profile')
 app.register_blueprint(messaging_bp)
 app.register_blueprint(home_bp)
-
-#not an actual route , this is just for testing if mongoDB is connected or not 
-@app.route('/dbtest')
-def dbtest():
-    try:
-        print("MONGO_URI:", app.config.get("MONGO_URI"))
-        print("mongo instance:", mongo)
-        print("mongo.db:", mongo.db)
-        mongo.db.command("ping")
-        return "Successfully connected to MongoDB!"
-    except Exception as e:
-        return f"Failed to connect to MongoDB: {str(e)}"
-@app.route('/')
-def home():
-    return render_template('home.html', current_user=current_user)
 
 if __name__ == '__main__':
     app.run(debug=True)
