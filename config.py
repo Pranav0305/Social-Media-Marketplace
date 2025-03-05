8ff233da12fa23f4531c425abc3c7b31c5b351a5
Annika Sinha <131460984+annikaasinha@users.noreply.github.com>
2025-03-05 10:19:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     
     MONGO_URI = os.environ.get("MONGO_URI") or "mongodb+srv://voldie1803:XsQqf28OaueJY11v@cluster0.fud6f.mongodb.net/socialMediaDB?retryWrites=true&w=majority"
     ENCRYPTION_KEY = os.environ.get("ENCRYPTION_KEY") or b"g8H2CMXcp92DfTNFKKi3IsL8o3SENR2OYnuG00yRBmU="
+    DEBUG = False
 
 #username - voldie1803
 #password - XsQqf28OaueJY11v
