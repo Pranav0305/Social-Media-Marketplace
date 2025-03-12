07e5643c5c8a978948216bcfbc8f6c64afb0deb3
Annika Sinha <131460984+annikaasinha@users.noreply.github.com>
2025-03-12 13:58:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "HelloWorld"
     
     MONGO_URI = os.environ.get("MONGO_URI") or "mongodb+srv://voldie1803:XsQqf28OaueJY11v@cluster0.fud6f.mongodb.net/socialMediaDB?retryWrites=true&w=majority"
-    ENCRYPTION_KEY = os.environ.get("ENCRYPTION_KEY") or b"g8H2CMXcp92DfTNFKKi3IsL8o3SENR2OYnuG00yRBmU="
+    ENCRYPTION_KEY = bytes.fromhex("8af3d8a815a38098cfea538a94986ed6571c97a326997a84")  
     DEBUG = False
 
 #username - voldie1803
